8db1d3fdae0cab7c4c10f5215c00ece9e306d704
Théo Roncalli <theo.roncalli@yahoo.fr>
2021-11-06 19:26:13 +0100

    Downloading human genome file

---
+++ Data/Genome/download.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget "http://labshare.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARgenomes/Human/GRCh38_Ensembl99_sparseD3_sjdbOverhang99/Homo_sapiens.GRCh38.dna.primary_assembly.fa"
