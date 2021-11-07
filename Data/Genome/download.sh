79af5898a76eb93edd605400ef0193f8ba688655
bio-TAGI <0mua6xvcm@relay.firefox.com>
2021-11-07 12:00:12 +0000

    :sparkles: Data/Genome/download.sh now retrieves genome annotations
    .

--- Data/Genome/download.sh
+++ Data/Genome/download.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Downloading genome..."
 wget "http://labshare.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARgenomes/Human/GRCh38_Ensembl99_sparseD3_sjdbOverhang99/Homo_sapiens.GRCh38.dna.primary_assembly.fa"
+echo "Done"
+
+echo "Downloading annotations..."
+wget "http://labshare.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARgenomes/Human/GRCh38_Ensembl99_sparseD3_sjdbOverhang99/Homo_sapiens.GRCh38.99.gtf"
+echo "Done"
