0bffa41fc3cb59ab2990718034bc620201ebae93
bio-TAGI <0mua6xvcm@relay.firefox.com>
2021-11-21 19:18:52 +0000

    :heavy_plus_sign: added seaborn dependency
    .

--- Python/devel.py
+++ Python/devel.py
@@ -1,4 +1,8 @@
 
+try:
+    from profile_binr import ProfileBin
+except:
+    print("\n ImportError (catched) : unable to import profile_binr")
 
 from tagi_rnaseq.parsing import *
 from tagi_rnaseq.normalization import *
@@ -14,3 +18,6 @@ metadata = get_metadata(x)
 # normalize the counts
 cpm = normalize_cpm(counts)
 log_cpm = log_transform(cpm)
+
+# most variable genes
+vargenes = pd.read_csv("var_counts.csv", index_col=0)
